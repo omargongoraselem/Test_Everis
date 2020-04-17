27a0a7ab8e8d826955bf7d6e6e505b3cffaaca6e
Marisol <reyes-espino-marisol@hotmail.com>
2020-04-17 15:23:44 -0500

    New project ConectNaturaleza with the corresponding constraints and resources

---
+++ iOS/practicas/ConectadoNaturaleza/ConectadoNaturaleza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConectadoNaturaleza
+//
+//  Created by Marisol Perez Rangel on 17/04/20.
+//  Copyright © 2020 Marisol Perez Rangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
