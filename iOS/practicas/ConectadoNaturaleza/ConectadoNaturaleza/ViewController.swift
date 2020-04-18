1b6f0d42a519dff9960ece3310f9c9f85524c445
Marisol <reyes-espino-marisol@hotmail.com>
2020-04-17 21:17:18 -0500

    Cocoapods dependencies added: Alamofire and ObjectMapper

--- iOS/practicas/ConectadoNaturaleza/ConectadoNaturaleza/ViewController.swift
+++ iOS/practicas/ConectadoNaturaleza/ConectadoNaturaleza/ViewController.swift
@@ -14,7 +14,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // Endpoint: https://reqres.in/api/register
+    // 1. Crear excepcion de seguridad.
+    // 2. Crear URL con el endpoint
+    // 3. Hacer request con la ayuda de URLSession
+    // 4. Transformar respuesta a diccionario
+    // 5. Ejecutar request
+    
+    private func fetchService() {
+        let endpointString = "https://reqres.in/api/register"
+        guard let endpoint = URL(string: endpointString) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: endpoint) { (data: Data?, _, error: Error?) in
+            if error != nil {
+                print("error")
+                return
+            }
+            guard let dataFromService = data, let dictionary = try? JSONSerialization.jsonObject(with: dataFromService, options: []) as? [String: Any] else {
+                return
+            }
+            print(dictionary["name"] as? String ?? "error2")
+        }
+    }
+    
+    
+    
+    
 }
 
